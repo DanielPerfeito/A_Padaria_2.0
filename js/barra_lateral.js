124732a86adde0e1bbcec85be22bd4afa4492bff
DanielPerfeito <dp.ribeiro.1996@gmail.com>
2024-06-15 00:04:17 -0300

    Estilização da seção de inicio e barra lateral.

---
+++ js/barra_lateral.js
@@ -0,0 +1,9 @@
+const barraLateral = document.getElementById("barra-lateral")
+
+function abrirMenu(){
+    barraLateral.style.width = "250px"
+}
+
+function fecharMenu(){
+    barraLateral.style.width = "0px"
+}
